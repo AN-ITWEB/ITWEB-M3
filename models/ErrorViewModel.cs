7400daa8b999eed9081aee5ac3ec01f7ff5fcf1e
Nicklas Nielsen <nnicklas92@gmail.com>
2018-12-16 10:51:35 +0100

    Changed project type

---
+++ models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ITWEB_M3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
